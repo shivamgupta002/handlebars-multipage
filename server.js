241d6b4f89e0c9a6cdfd3570d755f05286748fbc
shivamgupta002 <shivamgupta992909@gmail.com>
2024-03-29 11:36:50 +0530

    remove console

--- server.js
+++ server.js
@@ -22,30 +22,24 @@ app.use("/data/:username", async (req, res, next) => {
   const { username } = req.params;
   try {
     const response = await axios.get(`http://localhost:5000/data/${username}`);
-    // console.log(response);
     req.apiData = response.data; // Attach fetched data to req object
     next(); // Proceed to the next middleware/route handler
   } catch (error) {
-    // console.error("Error fetching data:", error);
     res.status(500).send("Error fetching data from API");
   }
 });
 
 // Read the Handlebars template file
 const templateSource = fs.readFileSync(templateFilePath, "utf8");
-// console.log(templateSource);
 
 // Compile the template
 const template = Handlebars.compile(templateSource);
-// console.log(template);
 
 // Read the JSON data
 const jsonData = fs.readFileSync("./constants/demo.json", "utf8");
-// console.log(jsonData);
 
 // Parse the JSON data
 const demoData = JSON.parse(jsonData);
-// console.log(demoData);
 
 // Render the template with the JSON data
 const renderedHtml = template(demoData);
@@ -59,14 +53,12 @@ const hbs = exphbs.create({
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+// fetching data of username
 app.get("/data/:username", (req, res) => {
   const { username } = req.params;
-  // console.log(username);
   const { apiData } = req; // Access the fetched data from req object
-  // console.log(apiData);
   // Render the template with the fetched data
   homePageData = apiData;
-  // console.log(homePageData);
   res.render("templates/template2/template_2", {
     homePageData: homePageData, // Pass only the response data to the template
   });
@@ -74,9 +66,9 @@ app.get("/data/:username", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  // res.render("home", { title: "Home" });
+  res.render("home", { title: "Home" });
   // res.render("home", { title: "Home", renderedHtml });
-  res.send(renderedHtml);
+  // res.send(renderedHtml);
 });
 
 app.get("/about", (req, res) => {
